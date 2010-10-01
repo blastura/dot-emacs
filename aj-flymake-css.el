cc72852c4e572d42bfabdce9fdff015126e6e8a7
Anton Johansson <anton.johansson@gmail.com>
2010-10-01 15:29:20 +0200

    Added CSS profile to flymake-css

--- aj-flymake-css.el
+++ aj-flymake-css.el
@@ -1,5 +1,6 @@
 ;; http://jigsaw.w3.org/css-validator/DOWNLOAD.html
 (defconst css-validator (expand-file-name "~/bin/css-validator"))
+(defconst css-validator-profile "css3")
 
 (defun flymake-css-init ()
   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
@@ -7,7 +8,7 @@
          (local-file  (file-relative-name
                        temp-file
                        (file-name-directory buffer-file-name))))
-    (list css-validator (list "-output gnu" (concat "file:" local-file)))))
+    (list css-validator (list "-output" "gnu" "-profile" css-validator-profile (concat "file:" local-file)))))
 
 (push '(".+\\.css$" flymake-css-init) flymake-allowed-file-name-masks)
 
